9941a0bab04e4d3b1eb5fd5665417503c60d5ace
Joe Zucco <josephzucco@gmail.com>
2021-10-17 13:13:54 -0400

    remove boilerplate and set up map component

---
+++ src/components/Map.js
@@ -0,0 +1,10 @@
+import GoogleMapReact from "google-map-react"
+
+const Map = ({center, zoom}) => {
+    return (
+        <div>
+            <h1>Map Component</h1>
+        </div>
+    )
+}
+export default Map
