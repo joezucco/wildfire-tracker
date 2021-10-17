f8ee5e8a25e19e3344442b2ceee9ed48c5b0f6a8
Joe Zucco <josephzucco@gmail.com>
2021-10-17 14:11:08 -0400

    import spinning loader

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import { useState, useEffect } from "react";
 import Map from "./components/Map";
+import Loader from './components/Loader'
 
 function App() {
+  const [eventData, setEventData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const nasaApi = "https://eonet.sci.gsfc.nasa.gov/api/v2.1/events";
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      setLoading(true);
+      const res = await fetch(nasaApi);
+      const { events } = await res.json();
+      setEventData(events);
+      setLoading(false);
+    };
+
+    fetchEvents();
+  }, []);
+
   return (
     <div className='App'>
-      <Map />
+      {!loading ? <Map eventData={eventData} /> : <Loader />}
     </div>
   );
 }
