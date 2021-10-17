d32f9d0332554b5eeb6668d12f725eaea83172d2
Joe Zucco <josephzucco@gmail.com>
2021-10-17 13:35:26 -0400

    set default props for google map react

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,10 +1,22 @@
-import GoogleMapReact from "google-map-react"
+import GoogleMapReact from "google-map-react";
 
-const Map = ({center, zoom}) => {
-    return (
-        <div>
-            <h1>Map Component</h1>
-        </div>
-    )
-}
-export default Map
+const Map = ({ center, zoom }) => {
+  return (
+    <div className='map'>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: AIzaSyDngO4GZm6TZOfQv0L2BDyAbUeqX17VOkI }}
+        defaultCenter={center}
+        defaultZoom={zoom}
+      ></GoogleMapReact>
+    </div>
+  );
+};
+
+Map.defaultProps = {
+  center: {
+    lat: 42.3265,
+    lng: -122.8756,
+  },
+  zoom: 6,
+};
+export default Map;
